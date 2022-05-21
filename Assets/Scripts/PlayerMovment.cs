950eeb3b0a08a88a7c1490b592b6de647f4f3597
psyGamer <psygamer21@gmail.com>
2022-05-21 20:07:07 +0200

    Initial commit

---
+++ Assets/Scripts/PlayerMovment.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerMovment : MonoBehaviour
+{
+    public float moveSpeed;
+    public float jumpHight;
+
+    private float horizontal;
+    private float jumpForce;
+
+    public Rigidbody2D rigidbody;
+
+    // Update is called once per frame
+    void FixedUpdate()
+    {
+        horizontal = Input.GetAxis("Horizontal") * Time.fixedDeltaTime * moveSpeed;
+
+
+        rigidbody.MovePosition(transform.position + new Vector3(horizontal, 0, 0));
+        Jump();
+    }
+
+    private void Jump()
+    {
+        if (Input.GetKey(KeyCode.Space))
+        {
+            rigidbody.velocity.y = transform.y * Time.fixedDeltaTime * jumpForce;
+        }
+    }
+}
